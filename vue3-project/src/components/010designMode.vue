3dd05e880a1d95d2d6f0a92d5945a56012d61956
cwh666 <15133266546@sohu.com>
2023-06-05 23:50:10 +0800

    feat: 设计模式

--- vue3-project/src/components/010designMode.vue
+++ vue3-project/src/components/010designMode.vue
@@ -5,6 +5,7 @@
     <button @click="classPattern">类模式</button>
     <button @click="singletonPattern">单例模式</button>
     <button @click="observerpPattern">观察者模式</button>
+    <button @click="publishSubscribe">发布订阅模式</button>
 </template>
 <script>
 
@@ -73,24 +74,128 @@ export default {
 
         //观察者模式
         const observerpPattern = () => {
+            //观察者主体
             class Subject {
-                constructor() { 
+                constructor() {
+                    //观察者列表
                     this.observers = []
                 }
+                //添加观察者
                 add (observer) {
                     this.observers.push(observer)
                 }
+                //通知观察者
+                notify () {
+                    this.observers.forEach(item => {
+                        console.log(item)
+                        item.updata()
+                    })
+                }
+                //移除观察者
+                remove (observer) {
+                    this.observers = this.observers.filter(item => item !== observer)
+                }
             }
 
+            //观察者个体
             class Observe {
-
+                constructor(name) {
+                    this.name = name
+                }
+                updata () {
+                    console.log('updata', this.name)
+                }
             }
 
             const subject = new Subject()
-            
+            const observer1 = new Observe('zhangsan')
+            const observer2 = new Observe('lisi')
+            subject.add(observer1)
+            subject.add(observer2)
+            console.log(subject.notify())
         }
 
 
+        //发布订阅模式
+        const publishSubscribe = () => {
+
+            // const PubSub = {
+            //     list: [],
+            //     // 发布
+            //     publish () {
+            //         this.list.forEach(item => item())
+            //     },
+            //     //订阅
+            //     subscribe (cb) {
+            //         this.list.push(cb)
+            //     }
+            // }
+
+
+            // function testA () {
+            //     console.log('testA')
+            // }
+            // function testB () {
+            //     console.log('testB')
+            // }
+
+
+            // PubSub.subscribe(testA)
+            // PubSub.subscribe(testB)
+
+            // PubSub.publish()
+
+
+
+            const PubSub = {
+                message: {},
+                // 发布
+                publish (type, data) {
+                    // this.list.forEach(item => item())
+                    if (!this.message[type]) return
+                    this.message[type].forEach(item => item(data))
+                },
+                //订阅
+                subscribe (type, cb) {
+                    if (!this.message[type]) {
+                        this.message[type] = [cb]
+                    } else {
+                        this.message[type].push(cb)
+                    }
+                },
+
+                //取消订阅
+                unSubscribe (type, cd) {
+                    if (!this.message[type]) return
+                    if (!cd) {
+                        // 取消全部订阅
+                        this.message[type] && (this.message[type].length = 0)
+                    } else {
+                        this.message[type] = this.message[type].filter(item => item !== cd)
+                    }
+                }
+            }
+
+
+            function testA (data) {
+                console.log('testA', data)
+            }
+            function testB (data) {
+                console.log('testB', data)
+            }
+
+
+            PubSub.subscribe('a', testA)
+            PubSub.subscribe('b', testB)
+
+            console.log(PubSub.message)
+            PubSub.publish('a', 'zhangsa')
+            PubSub.publish('b', '12344')
+             //取消订阅
+            PubSub.unSubscribe('b', testB)
+            console.log('取消订阅',PubSub.message)
+            PubSub.publish('b', '12344')
+        }
 
 
         return {
@@ -98,7 +203,8 @@ export default {
             prototypePattern,
             classPattern,
             singletonPattern,
-            observerpPattern
+            observerpPattern,
+            publishSubscribe
         }
     },
 }
--- vue3-project/src/components/011arithmetic.vue
+++ vue3-project/src/components/011arithmetic.vue
@@ -1,6 +1,9 @@
 <template>
     <h1>11算法</h1>
     <button @click="bubbleSort">冒泡排序</button>
+    <button @click="selectionSort">选择排序</button>
+    <button @click="insertionSorting">插入排序</button>
+    <button @click="quickSort">快速排序</button>
 </template>
 <script>
 
@@ -11,22 +14,91 @@ export default ({
             console.log('冒泡排序')
             Array.prototype.bubble = function () {
                 console.log(this)
-                // debugger
-                for (let j = 0; j < this.length - 1; j++) {
-                    if (this[j] > this[j + 1]) {
-                        const temp = this[j]
-                        this[j] = this[j + 1]
-                        this[j + 1] = temp
+                debugger;
+                for (let i = 0; i < this.length - 1; i++) {
+                    for (let j = 0; j < this.length - 1 - i; j++) {
+                        if (this[j] > this[j + 1]) {
+                            const temp = this[j]
+                            this[j] = this[j + 1]
+                            this[j + 1] = temp
+                        }
                     }
                 }
+
                 console.log(this)
             }
             const arr = [4, 3, 8, 1, 9, 6]
             arr.bubble()
 
         }
+
+
+        //选择排序
+        const selectionSort = () => {
+            console.log('选择排序')
+            Array.prototype.selection = function () {
+                console.log(this)
+                debugger;
+                for (let i = 0; i < this.length - 1; i++) {
+                    let indexMin = i;
+                    for (let j = i; j < this.length; j++) {
+                        if (this[j] < this[indexMin]) {
+                            indexMin = j
+                        }
+                    }
+                    if (indexMin !== i) {
+                        const temp = this[i]
+                        this[i] = this[indexMin]
+                        this[indexMin] = temp
+                    }
+                }
+
+                console.log(this)
+            }
+            const arr = [4, 3, 8, 1, 9, 6]
+            arr.selection()
+        }
+
+
+
+        //插入排序
+        const insertionSorting = () => {
+            Array.prototype.insertion = function () {
+                console.log(this)
+                debugger;
+                for (let i = 1; i < this.length; i++) {
+                    const temp = this[i]
+                    let j = i;
+                    while (j > 0) {
+                        if (this[j - 1] > temp) {
+                            this[j] = this[j - 1]
+                        } else {
+                            break
+                        }
+                        j -= 1
+                    }
+                    this[j] = temp
+                }
+                console.log(this)
+            }
+            const arr = [4, 3, 8, 1, 9, 6]
+            arr.insertion()
+        }
+
+
+
+        //快速排序
+        const quickSort = () => {
+            console.log('快速排序')
+        }
+
+
+
         return {
-            bubbleSort
+            bubbleSort,
+            selectionSort,
+            insertionSorting,
+            quickSort
         }
     },
 })
