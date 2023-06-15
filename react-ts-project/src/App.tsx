46413aa5aa7e22f508a239ed1ba2ac84e1914000
cwh666 <15133266546@sohu.com>
2023-06-15 15:00:13 +0800

    feat: 初始化

--- react-ts-project/src/App.tsx
+++ react-ts-project/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './App.css'
 import Hello from './components/Hello'
 import LikeButton from './components/useState'
+import ProjectApp from './components/project/index'
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+          <ProjectApp></ProjectApp>
           <Hello mes="TS组件"></Hello>
           <LikeButton></LikeButton>
     </div>
---
+++ react-ts-project/src/components/project/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.css'
+
+const ProjectApp = () => {
+  return <div className='project_box'>project</div>
+}
+
+export default ProjectApp
